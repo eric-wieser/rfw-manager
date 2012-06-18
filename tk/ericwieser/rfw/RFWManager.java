d2c8835c0da49014c9326b2662fd9f20ed27b8fa
Eric Wieser <wieser.eric@gmail.com>
2012-06-18 19:55:51 +0100

    Added command aliases

--- tk/ericwieser/rfw/RFWManager.java
+++ tk/ericwieser/rfw/RFWManager.java
@@ -1,10 +1,14 @@
 package tk.ericwieser.rfw;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -37,11 +41,43 @@ public class RFWManager extends JavaPlugin implements Listener {
 		getCommand("teamname").setExecutor(teamName);
 		getCommand("teamcolor").setExecutor(teamColor);
 		getCommand("teamlist").setExecutor(teamList);
+		getCommand("team").setExecutor(new CommandExecutor() {
+			@Override
+			public boolean onCommand(CommandSender sender, Command command,
+			        String label, String[] args) {
+				if(args.length < 1) return true;
+				String subcommand = args[0];
+				String[] newargs = Arrays.copyOfRange(args, 1, args.length);
+
+				command = RFWManager.this.getCommand("team"+subcommand);
+				if(command != null) {
+					command.execute(sender, "team "+subcommand, newargs);
+					return true;
+				}
+				return false;
+			}
+		});
 		
 		getCommand("gamestart").setExecutor(gameStart);
 		getCommand("gamestop").setExecutor(gameStop);
 		getCommand("gamelist").setExecutor(gameList);
+		getCommand("game").setExecutor(new CommandExecutor() {
+			@Override
+			public boolean onCommand(CommandSender sender, Command command,
+			        String label, String[] args) {
+				if(args.length < 1) return true;
+				String subcommand = args[0];
+				String[] newargs = Arrays.copyOfRange(args, 1, args.length);
 
+				command = RFWManager.this.getCommand("game"+subcommand);
+				if(command != null) {
+					command.execute(sender, "game "+subcommand, newargs);
+					return true;
+				}
+				return false;
+			}
+		});
+		
 		World df = getServer().getWorld("direct_fire");
 		if(df != null) games.add(new Game(this, df));
 	};
